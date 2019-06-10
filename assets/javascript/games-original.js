bf0f61d97eb05d89389cd22cfcaac9655092534b
Liz Guenthner <liz.guenthner@t-3.com>
2019-06-09 22:58:30 -0500

    create new js files to compare functionality

---
+++ assets/javascript/games-original.js
@@ -0,0 +1,66 @@
+// YouTube example https://www.youtube.com/watch?v=SvPtEr9tblU
+
+    // create array of words
+    var words = ["davis", "coltrane", "mingus", "rollins", "ellington"];
+    var game = 0;
+    var wins = 0;
+    var losses = 0;
+
+    // pick random word from array
+    var word = words[Math.floor(Math.random() * words.length)];
+
+    // setup answerArray to show how many letters in the chosen word
+    // using underscores
+    var answerArray = [];
+
+    for (var i = 0; i < word.length; i++) {
+      answerArray[i] = "_";
+    }
+
+    // create a variable to hold the number of remainingLetters to be guessed
+    var remainingLetters = word.length;
+
+    // **** MAIN GAME LOOP ****
+
+    // while there are letters left to be guessed
+    while (remainingLetters > 0) {
+      // show player their progress
+      alert(answerArray.join(" ")); // puts spaces in between each space or letter
+      //$( "#currentWord" ).append( answerArray.join(" ") );
+
+      // get a guess from player
+      var userGuess = prompt("Guess a letter, or click cancel to stop playing.");
+
+      // if guess is blank or cancel
+      if (userGuess == null) {
+      //  exit game loop
+        break;
+      // if guess is more than one letter or no letters
+      } else if (userGuess.length !== 1) {
+      //  alert to guess single letter only
+        alert("Please enter a single letter.");
+        // $( "#flash-message" ).append( "Please enter a single letter." );
+      //   valid guess 
+      } else {
+      //  update game state with guess
+        for (var j = 0; j < word.length; j++) {
+          //  if letter they guessed is in the word
+          //  at that point or index
+          if (word[j] === userGuess) {
+          //update answer array with the letter they guessed
+          //at that point or index
+            answerArray[j] = userGuess;
+            //  subtract 1 from remaining letters
+            remainingLetters--;
+          }
+        }
+      }
+      //  **** END OF GAME LOOP ****
+    }
+    // let player know the word 
+    alert(answerArray.join(" "));
+    //$( "#currentWord" ).append( answerArray.join(" ") );
+
+    // congratulate the player
+    alert("Good job! The answer was " + word);
+    //$( "#flash-message" ).append( "Good job! The answer was " + word );
